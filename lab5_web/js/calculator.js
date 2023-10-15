b381f7c89b87421479147be5355443ce2a3ad051
SodolynskyiVadym <117468674+SodolynskyiVadym@users.noreply.github.com>
2023-10-15 17:13:23 +0300

    Update calculator.js

--- lab5_web/js/calculator.js
+++ lab5_web/js/calculator.js
@@ -1,11 +1,13 @@
 function calculate() {
     let length = document.getElementById("length").value;
     let width = document.getElementById("width").value;
-    if (length === "" || width === "") {
+    if (parseFloat(length) < 0 || parseFloat(width) < 0){
+        specifyValue(document.getElementsByTagName("span"), "Error data");
+    }else if (length === "" || width === "") {
         specifyValue(document.getElementsByTagName("span"), "No data entered");
     } else if(length === "0" || width === "0"){
         specifyValue(document.getElementsByTagName("span"), "0");
-    } else if (!(length === "0" || width === "0")) {
+    } else{
         length = parseFloat(length);
         width = parseFloat(width);
 
